e7510faac25a1c5058edd071968cdc07cee9fd64
ZacharyWittmann <wittmannzachary@gmail.com>
2024-06-11 19:39:18 -0600

    Initial commit

---
+++ draw_shapes.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "draw.h"
+
+/* Prints a size x size square whose left col is at startCol */
+void print_square(int leftCol, int size)
+{
+  int i, j;
+  int endCol = leftCol + size;
+  for (int row = 0; row < size; row++){
+    int col;
+    for (col = 0; col < leftCol; col++) putchar(' ');
+    for (       ; col < endCol;  col++) putchar('*');
+    putchar('\n');
+  }
+}
+
+// Prints a triangle of specified height whose left edge is at col leftCol
+void print_triangle(int leftCol, int size)
+{
+  for (int row = 0; row <= size; row++) {
+    int minCol = leftCol + size - row, maxCol = leftCol + size + row;
+    int col;
+    for (col = 0; col < minCol; col++) putchar(' ');
+    for (       ; col <= maxCol; col++) putchar('*');
+    putchar('\n');
+  }
+}
+
+// Prints an arrow of specified height whose left edge is at col leftCol
+void print_arrow(int leftCol, int size)
+{
+  for (int row = 0; row <= size; row++) {
+    int minCol = leftCol + size - row, maxCol = leftCol + size + row;
+    int col;
+    for (col = 0; col < minCol; col++) putchar(' ');
+    for (       ; col <= maxCol; col++) putchar('*');
+    putchar('\n');
+  }
+  int endCol = leftCol + size;
+  int midGap = (size+1)/2;
+  for (int row = 0; row < size; row++){
+    int col;
+    for (col = 0; col < leftCol + midGap; col++) putchar(' ');
+    for (       ; col < endCol + midGap;  col++) putchar('*');
+    putchar('\n');
+  }
+}
+
+  
+
